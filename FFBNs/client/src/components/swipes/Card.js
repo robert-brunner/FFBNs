7d30a5e8193a630589546d31958af436ce1631c3
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2022-12-12 10:50:32 -0500

    greened out card deck

--- FFBNs/client/src/components/swipes/Card.js
+++ FFBNs/client/src/components/swipes/Card.js
@@ -1,58 +1,58 @@
-import React from "react";
-import { animated, interpolate } from "react-spring/";
-import Carousel from "nuka-carousel";
-// import { getRandomUser } from "../../managers/UserProfileManager";
-// import { User } from "../users/User"
-// import { useEffect } from "react";
+// import React from "react";
+// import { animated, interpolate } from "react-spring/";
+// import Carousel from "nuka-carousel";
+// // import { getRandomUser } from "../../managers/UserProfileManager";
+// // import { User } from "../users/User"
+// // import { useEffect } from "react";
 
-// export const RandomUser = () => {
-//     const [user, setUser] = useState([]);
+// // export const RandomUser = () => {
+// //     const [user, setUser] = useState([]);
 
-//     const getUsers = () => {
-//         getRandomUser().then ( all => setUser(all))
-//     };
-//     useEffect(()=>{
-//         getUsers();
-//     }, []);}
-class Card extends React.Component {
-  render() {
-    const { i, x, y, rot, scale, trans, cards, bind, objs } = this.props;
-    const { name, age, distance, text, pics } = objs[i];
+// //     const getUsers = () => {
+// //         getRandomUser().then ( all => setUser(all))
+// //     };
+// //     useEffect(()=>{
+// //         getUsers();
+// //     }, []);}
+// class Card extends React.Component {
+//   render() {
+//     const { i, x, y, rot, scale, trans, cards, bind, objs } = this.props;
+//     const { name, age, distance, text, pics } = objs[i];
 
-    return (
+//     return (
     
-      <animated.div className="OuterBox"
-        key={i}
-        style={{
-          transform: interpolate(
-            [x, y],
-            (x, y) => `translate3d(${x}px,${y}px,0)`
-          )
-        }}
-      >
-        <animated.div className="InnerBox"
-          {...bind(i)}
-          style={{
-            transform: interpolate([rot, scale], trans)
-          }}
-        >
-          <div className="card">
-            <Carousel className="Carousel">
-              {pics.map(pic => (
-                <img className="CarouselImg" src={pic} alt="profilePicture" />
-              ))}
-            </Carousel>
-            <h4 className="DisplayName">{name},</h4>
-            <p className="Interest">{age}</p>
-          </div>
-        </animated.div>
-      </animated.div>
+//       <animated.div className="OuterBox"
+//         key={i}
+//         style={{
+//           transform: interpolate(
+//             [x, y],
+//             (x, y) => `translate3d(${x}px,${y}px,0)`
+//           )
+//         }}
+//       >
+//         <animated.div className="InnerBox"
+//           {...bind(i)}
+//           style={{
+//             transform: interpolate([rot, scale], trans)
+//           }}
+//         >
+//           <div className="card">
+//             <Carousel className="Carousel">
+//               {pics.map(pic => (
+//                 <img className="CarouselImg" src={pic} alt="profilePicture" />
+//               ))}
+//             </Carousel>
+//             <h4 className="DisplayName">{name},</h4>
+//             <p className="Interest">{age}</p>
+//           </div>
+//         </animated.div>
+//       </animated.div>
       
-    );
-  }
-}
+//     );
+//   }
+// }
 
-export default Card;
+// export default Card;
 
 
-/* get all for users and put them in state - feed*/
+// /* get all for users and put them in state - feed*/
