942612951e24c96f24f0d6c76b278540f77d1565
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2022-12-16 15:54:02 -0500

    temp save and push

--- FFBNs/client/src/components/users/UserProfilePictures.js
+++ FFBNs/client/src/components/users/UserProfilePictures.js
@@ -1,18 +1,31 @@
 import React, {useState} from "react";
+import Axios from "axios";
+
 
 
 
 
 function UserProfilePictures() {
 
-    const uploadImage = (files) => {
-        console.log(files [0]);
+    const [imageSelected, setImageSelected] = useState ("")
+
+    const uploadImage = () => {
+        const formData = new FormData()
+        formData.append("file", imageSelected)
+        formData.append("upload_preset", "cPawFilePics")
+
+        Axios.post("http://api.cloudinary.com/v1_1/dpwgvs3m0/image/upload", formData)
+        .then((response)=>console.log(response))
     }
     return (
     <>
     <div>
-        <input type="file" onChange={(event)=> {uploadImage(event.target.files)}}/>
-
+        <input 
+            type="file" onChange={(event)=> {
+                setImageSelected(event.target.files[0]);
+            }}
+        />
+        <button onClick={uploadImage}>UploadImage</button>
     </div>
     </>
     );
