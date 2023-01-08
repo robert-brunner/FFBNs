ba058ea423f4c9719811c13ddf6fba4a1ea5f446
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2023-01-08 15:17:52 -0500

    pushing up with some useful sql commands

---
+++ sQL with jordan.sql
@@ -0,0 +1,27 @@
+﻿--Select * From Dog
+--Select * From Swipes
+
+--Insert Into Swipes (DogId, OtherDogId, [Like]) VALUES (4,1,1)
+
+--delete from swipes
+
+--select * from dog
+
+
+--select s.DogId, s.OtherDogId, s.[Like], d.Username as 'Logged in Dog', do.Username as 'Dog that Got Liked' from swipes s join dog d on s.dogId = d.Id join dog do on s.OtherDogId = do.Id WHERE s.[Like]=1
+
+--select * from dog
+
+SELECT do.Id AS 'Dog You Liked Id', do.UserName AS 'Dog You Liked Name'  
+                                   FROM Dog d 
+                             Right JOIN Swipes ON
+                                        d.Id = Swipes.DogId 
+                                   JOIN Dog do on Swipes.OtherDogId = do.Id 
+                                  WHERE Swipes.DogId = 1 
+                       INTERSECT SELECT d.Id AS     
+                                        'DogWho Liked You', d.UserName AS 'Dog Who Liked You Name'  
+                                   FROM Dog d 
+                             Right JOIN Swipes ON
+                                        d.Id = Swipes.DogId 
+                                   JOIN Dog do on Swipes.OtherDogId = do.Id 
+                                  WHERE Swipes.OtherDogId = 1
