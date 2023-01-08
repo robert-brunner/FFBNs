b7f5a2ecd79809eb67a32f53cca735e01807830f
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2023-01-08 15:12:51 -0500

    minor changes- NEW BRANCH

--- FFBNs/client/src/managers/UserProfileManager.js
+++ FFBNs/client/src/managers/UserProfileManager.js
@@ -37,68 +37,18 @@ const apiUrl = "https://localhost:5001";
 };
   
 
-//     const publicId = response.data.public_id = () => {
-//         return fetch (`${apiUrl}/api/UserProfile`, {
-//             method: "POST",
-//             headers: {
-//           "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(userObject),
-//   })
-//       .then((response) => response.json())
-//       .then((savedUserProfile) => {
-//           localStorage.setItem("userProfile", JSON.stringify(savedUserProfile))
-//       });
-//   };
-//----------------------------------------
-// //gets the current userProfile information
-// export const getCurrentUser = () => {
-//   const currentUser = localStorage.getItem("UserProfile");
-
-//   return JSON.parse(currentUser);  //JSON.parse()  the local user object coming back from API to use properties of that object
-// };
-
-
-// I need to affix publicID from UploadProfilePictures to the update UserPawFile so I can store the hash data from my picture in the database- this is the method that I think I need to use.  
-
-// Function that sends a POST request with the public id string to the server
-// function sendPublicIdToSQL(publicId, userId) {
-//   Axios.post(`/server/endpoint/${userId}`, {
-//     publicId: publicId
-//   })
-//   .then(response => {
-//     // Do something with the response
-//   })
-//   .catch(error => {
-//     // Handle the error
-//   });
-// }
-
-// export const UpdateDog = (user) => {
-//   // Call the sendPublicIdToSQL function and pass the public id and user id as arguments
-//   sendPublicIdToSQL(user.profilePicture, user.id);
-
-//   return fetch(`${apiUrl}/api/userProfile/${user.id}`, {
-//     method: "PUT",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(user),
-//   }); 
-// }
-
 
 export const UpdateDog = (user) => {
-   return fetch(`${apiUrl}/api/userProfile/${user.id}`, {
-     method: "PUT",
+  return fetch(`${apiUrl}/api/userProfile/${user.id}`, {
+    method: "PUT",
      headers: {
        "Content-Type": "application/json",
      },
      body: JSON.stringify(user),
    }) 
-       
- }
-
+   
+  }
+  
  //ideally Like should put a Like on the User in SQL-C#
  //Onclick should sendLike to the below method which in turn should add a value of "like" to Dog in SQL; 
  // if both Dogs have a key of like than a match should be generated
@@ -107,39 +57,89 @@ export const UpdateDog = (user) => {
      method: "PUT",
      headers: {
        "Content-Type": "application/json",
-     },
+      },
      body: JSON.stringify(user),
-   }) 
-       
- }
-
-
-
-//uses the Get UserProfile Fetch Command that returns a randomized sql query
-export const getRandomUser = () => {
+    }) 
+    
+  }
+  
+  
+  
+  //uses the Get UserProfile Fetch Command that returns a randomized sql query
+  export const getRandomUser = () => {
     return fetch(`${apiUrl}/api/UserProfile`)//http GET request or  `/api/userProfile`
     .then((res) => res.json())
-};
-export const GetUllPawFiles = () => {
+  };
+  export const GetUllPawFiles = () => {
     return fetch(`${apiUrl}/api/UserProfile/GetAll`)//http GET request or  `/api/userProfile`
     .then((res) => res.json())
-};
-
-
-export const getUserById = (userId) => {
-  return fetch(`${apiUrl}/api/UserProfile/${userId}`)//http GET request or  `/api/userProfile`
-  .then((res) => res.json())
-};
-
-export const getCurrentUser = () => {
-  const currentUser = localStorage.getItem('userProfile');
-  const CurrentUserObject = JSON.parse(localStorage.getItem('userProfile').id)
-  return CurrentUserObject
-};
-
-
-
-
+  };
+  
+  
+  export const getUserById = (userId) => {
+    return fetch(`${apiUrl}/api/UserProfile/${userId}`)//http GET request or  `/api/userProfile`
+    .then((res) => res.json())
+  };
+  
+  export const getCurrentUser = () => {
+    const currentUser = localStorage.getItem('userProfile');
+    const CurrentUserObject = JSON.parse(localStorage.getItem('userProfile').id)
+    return CurrentUserObject
+  };
+  
+  
+  
+  
+  //     const publicId = response.data.public_id = () => {
+  //         return fetch (`${apiUrl}/api/UserProfile`, {
+  //             method: "POST",
+  //             headers: {
+  //           "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(userObject),
+  //   })
+  //       .then((response) => response.json())
+  //       .then((savedUserProfile) => {
+  //           localStorage.setItem("userProfile", JSON.stringify(savedUserProfile))
+  //       });
+  //   };
+  //----------------------------------------
+  // //gets the current userProfile information
+  // export const getCurrentUser = () => {
+  //   const currentUser = localStorage.getItem("UserProfile");
+  
+  //   return JSON.parse(currentUser);  //JSON.parse()  the local user object coming back from API to use properties of that object
+  // };
+  
+  
+  // I need to affix publicID from UploadProfilePictures to the update UserPawFile so I can store the hash data from my picture in the database- this is the method that I think I need to use.  
+  
+  // Function that sends a POST request with the public id string to the server
+  // function sendPublicIdToSQL(publicId, userId) {
+  //   Axios.post(`/server/endpoint/${userId}`, {
+  //     publicId: publicId
+  //   })
+  //   .then(response => {
+  //     // Do something with the response
+  //   })
+  //   .catch(error => {
+  //     // Handle the error
+  //   });
+  // }
+  
+  // export const UpdateDog = (user) => {
+  //   // Call the sendPublicIdToSQL function and pass the public id and user id as arguments
+  //   sendPublicIdToSQL(user.profilePicture, user.id);
+  
+  //   return fetch(`${apiUrl}/api/userProfile/${user.id}`, {
+  //     method: "PUT",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(user),
+  //   }); 
+  // }
+  
 /* 
 
 THE PROBLEM-
@@ -154,14 +154,14 @@ My code is a mess- what do you do when you create a new method
 -- Back End --
 
 1.) ...Controller 
-    [Http] NewCommand
-    public IActionResult NewCommand(x,y) 
+[Http] NewCommand
+public IActionResult NewCommand(x,y) 
 
 2.) ...Repository 
-    public NewCommand(x,y)
+public NewCommand(x,y)
 
 3.) I...Repository
-    NewCommand(x,y)
+NewCommand(x,y)
 
 
 ---Front End---
