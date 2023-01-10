011a649faf37271e73a05645be908af33b43516b
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2023-01-09 19:30:02 -0500

    working login carousel

--- FFBNs/client/src/components/loginSlider/LSlider.js
+++ FFBNs/client/src/components/loginSlider/LSlider.js
@@ -3,13 +3,34 @@ import './LSlider.scss'
 
 export const LoginSlider = () => {
   return (
-    <div className="slider">
-      <div className="slide-track">
-        <div className="slide">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+    <div className="container">
+      <div id="carousel">
+        <div>
+          <img src="https://res.cloudinary.com/dpwgvs3m0/image/upload/v1673297783/ebnzd1aaofamhkcyo1bs.jpg" />
         </div>
-        <div className="slide">
-          <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+        <div>
+          <img src="https://www.hepper.com/wp-content/uploads/2021/11/rsz_shutterstock_1402786178.jpg" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dpwgvs3m0/image/upload/v1671422855/z1mhftorqnorrxk2hidx.jpg" />
+        </div>
+        <div>
+          <img src="http://cdn.akc.org/content/article-body-image/cavkingcharlessmalldogs.jpg" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dpwgvs3m0/image/upload/v1671423161/tlnorwcigyolbytgp8rx.jpg" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dpwgvs3m0/image/upload/v1671422949/x8joeblznvlt1izhqpkc.jpg" />
+        </div>
+        <div>
+          <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=1200:*" />
+        </div>
+        <div>
+          <img src="https://res.cloudinary.com/dpwgvs3m0/image/upload/v1671420265/mphi9lfxn3kpe1jvatmj.jpg" />
+        </div>
+        <div>
+          <img src="https://media-be.chewy.com/wp-content/uploads/2022/09/27100424/cavalier-king-charles-spaniel-cute-dogs.jpg" />
         </div>
       </div>
     </div>
@@ -30,6 +51,17 @@ export const LoginSlider = () => {
 
 
 
+
+    // <div className="slider">
+    //   <div className="slide-track">
+    //     <div className="slide">
+    //       <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/1.png" height="100" width="250" alt="" />
+    //     </div>
+    //     <div className="slide">
+    //       <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/7.png" height="100" width="250" alt="" />
+    //     </div>
+    //   </div>
+    // </div>
 
 
 
